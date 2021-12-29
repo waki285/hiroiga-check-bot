e04997c1cdee1a3e414eb79be023612e7b721c24
waki285 <suzuneu@suzuneu.com>
2021-12-29 03:31:33 +0000

    ✨ feat: add context menu check

--- src/index.js
+++ src/index.js
@@ -40,9 +40,9 @@ client.on("messageCreate", async (message) => {
         method: "get"
       });
       if (data.status !== "success") return `${i + 1}番目の画像: 失敗しました。`;
-      if (data.found) 
+      if (data.found)
         return `${i + 1}番目の画像: **${data.count}**個の画像が見つかりました。\n${data.resulturl}`;
-        else return `${i + 1}番目の画像: 画像が見つかりませんでした。`;
+      else return `${i + 1}番目の画像: 画像が見つかりませんでした。`;
     } catch {
       return `${i + 1}番目の画像: 失敗しました。`;
     }
@@ -54,5 +54,39 @@ client.on("messageCreate", async (message) => {
     ]
   });
 });
+
+client.on("interactionCreate", async (i) => {
+  if (!i.isMessageContextMenu()) return;
+  /** @type {Message<true>} */
+  const ref = await i.channel.messages.fetch(i.targetId);
+  if (i.commandName === "チェック") {
+    if (!ref.attachments.size && !ref.embeds.filter(x => x.type === "image").length) return i.reply({ content: "そのメッセージには画像がありません。", ephemeral: true });
+    const imageUrls = [];
+    for (const url of ref.attachments.map(x => x.url)) imageUrls.push(url);
+    for (const url of ref.embeds.filter(x => x.type === "image").map(x => x.url)) imageUrls.push(url);
+    await i.deferReply();
+    const promiseArr = imageUrls.map(async (image, i) => {
+      try {
+        const { data } = await axios({
+          url: `https://api.irucabot.com/imgcheck/check_url?url=${encodeURIComponent(image)}`,
+          responseType: "json",
+          method: "get"
+        });
+        if (data.status !== "success") return `${i + 1}番目の画像: 失敗しました。`;
+        if (data.found)
+          return `${i + 1}番目の画像: **${data.count}**個の画像が見つかりました。\n${data.resulturl}`;
+        else return `${i + 1}番目の画像: 画像が見つかりませんでした。`;
+      } catch {
+        return `${i + 1}番目の画像: 失敗しました。`;
+      }
+    });
+    const fullResult = await Promise.all(promiseArr);
+    i.followUp({
+      content: null, embeds: [
+        { description: fullResult.join("\n") }
+      ]
+    });
+  }
+})
 
 client.login(process.env.BOT_TOKEN)
