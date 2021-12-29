d549c696528c67954e190f8abb55beee9e642200
waki285 <suzuneu@suzuneu.com>
2021-12-29 03:33:59 +0000

    ✨ feat: add context menu ghost-check

--- src/index.js
+++ src/index.js
@@ -86,6 +86,33 @@ client.on("interactionCreate", async (i) => {
         { description: fullResult.join("\n") }
       ]
     });
+  } else if (i.commandName === "ゴーストチェック") {
+    if (!ref.attachments.size && !ref.embeds.filter(x => x.type === "image").length) return i.reply({ content: "そのメッセージには画像がありません。", ephemeral: true });
+    const imageUrls = [];
+    for (const url of ref.attachments.map(x => x.url)) imageUrls.push(url);
+    for (const url of ref.embeds.filter(x => x.type === "image").map(x => x.url)) imageUrls.push(url);
+    await i.deferReply({ ephemeral: true });
+    const promiseArr = imageUrls.map(async (image, i) => {
+      try {
+        const { data } = await axios({
+          url: `https://api.irucabot.com/imgcheck/check_url?url=${encodeURIComponent(image)}`,
+          responseType: "json",
+          method: "get"
+        });
+        if (data.status !== "success") return `${i + 1}番目の画像: 失敗しました。`;
+        if (data.found)
+          return `${i + 1}番目の画像: **${data.count}**個の画像が見つかりました。\n${data.resulturl}`;
+        else return `${i + 1}番目の画像: 画像が見つかりませんでした。`;
+      } catch {
+        return `${i + 1}番目の画像: 失敗しました。`;
+      }
+    });
+    const fullResult = await Promise.all(promiseArr);
+    i.followUp({
+      content: null, embeds: [
+        { description: fullResult.join("\n") }
+      ]
+    });
   }
 })
 
